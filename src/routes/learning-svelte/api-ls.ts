d2304b5f913c6b321eb132dafd2dfe0164c08105
dave <dave@davecode.me>
2021-10-05 15:30:13 -0400

    fix this

--- src/routes/learning-svelte/api-ls.ts
+++ src/routes/learning-svelte/api-ls.ts
@@ -1,8 +1,8 @@
 import { RequestHandler } from '@sveltejs/kit';
-import { readdir } from 'fs-extra';
+import * as fs from 'fs-extra';
 
 export const get: RequestHandler = async ({}) => {
-  const files = (await readdir('src/routes/learning-svelte'))
+  const files = (await fs.readdir('src/routes/learning-svelte'))
     .filter((x) => x.endsWith('.svelte'))
     .map((x) => x.replace('.svelte', ''))
     .filter((x) => x !== 'index');
