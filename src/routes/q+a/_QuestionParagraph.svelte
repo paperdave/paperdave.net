6d45d496d0b181aaeb4f351645549f700fdb0947
dave <dave@davecode.me>
2021-10-12 18:41:28 -0400

    fix qpara

--- src/routes/q+a/_QuestionParagraph.svelte
+++ src/routes/q+a/_QuestionParagraph.svelte
@@ -2,33 +2,6 @@
   import { QuestionParagraph } from '$lib/structures';
 
   export let paragraph: QuestionParagraph;
-
-  let RootElement = 'p';
-  let what = paragraph.message;
-  let props = {};
-
-  if (what.startsWith('RootElement=')) {
-    RootElement = /RootElement=(.*?)\{/.exec(what)[1];
-    let depth = 1;
-    let jsonString;
-    for (let i = 13 + RootElement.length; i < what.length; i++) {
-      if (what[i] === '}') {
-        depth--;
-        if (depth === 0) {
-          jsonString = what.slice(12 + RootElement.length, i + 1);
-          what = what.slice(12 + RootElement.length + jsonString.length);
-          break;
-        }
-      }
-      if (what[i] === '{') {
-        depth++;
-      }
-    }
-    if (!jsonString) {
-      throw new Error('Could not find JSON string');
-    }
-    props = JSON.parse(jsonString);
-  }
 </script>
 
 <main class:q={paragraph.isQuestion()} class:a={paragraph.isAnswer()}>
