5599806bad39c7973c3688de8088ebead778bdc6
dave <dave@davecode.me>
2021-10-20 11:26:36 -0400

    homepage craze

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -16,28 +16,45 @@
       other websites.
     </p>
 
+    <p>pages (i'll make this homepage nicer later)</p>
     <ul>
-      <li><a href="/discord">chat on discord</a></li>
-      <li><a href="/matrix">chat on matrix</a></li>
+      <li><a href="/now">what's now</a></li>
+      <br />
+      <li class="wip"><del>videos</del> - not ready</li>
+      <li><a href="/music">music</a></li>
+      <li class="wip"><del>video games</del> - not ready</li>
+      <li class="wip"><del>applications</del> - not ready</li>
+      <li class="wip"><del>nerd gear</del> - not ready</li>
+      <li><a href="/q+a">question answer</a></li>
+      <br />
+      <li class="wip"><del>bits & fragments</del> - not ready</li>
+      <li class="wip"><del>the graveyard</del> - not ready</li>
+      <br />
+      <li class="wip"><del>my journal</del> - not ready</li>
+      <li class="wip"><del>my brain</del> - not ready</li>
+      <li class="wip"><del>my treehouse</del> - not ready</li>
+      <br />
+      <li class="wip"><del>amatuer mode</del> - not ready</li>
+    </ul>
+
+    <p>external links</p>
+    <ul>
+      <li>
+        <a href="/discord">chat on discord</a>
+        <span style="font-size:0.8em;">(or <a href="/matrix">matrix</a>)</span>
+      </li>
       <li><a href="mailto:dave@davecode.me">send email</a></li>
       <li><a href="https://youtube.com/davecode">youtube</a></li>
       <li><a href="https://www.tiktok.com/@dave_caruso">tiktok</a></li>
-      <!-- <li><a href="https://twitch.tv/davecaruso">twitch</a></li>
-      <li><a href="https://twitter.com/davecode_me">twitter</a></li>
-      <li><a href="https://instagram.com/davecode.me">instagram</a></li> -->
+      <li><a href="https://twitch.tv/davecaruso">twitch</a> (not touched much)</li>
+      <li><a href="https://twitter.com/davecode_me">twitter</a> (not touched much)</li>
+      <li><a href="https://instagram.com/davecode.me">instagram</a> (not touched much)</li>
       <li><a href="/donate">give me chocolate</a></li>
     </ul>
 
-    <p>existing pages</p>
+    <br />
+    <br />
     <ul>
-      <li><a href="/q+a">question answer</a></li>
-      <li><a href="/music">the music</a></li>
-      <li><a href="/brain">brain thoughts</a></li>
-      <li><a href="/now">now</a></li>
-      <li><a href="/blog/learning-svelte">blog: learning svelte</a></li>
-      <li>more coming very soon.</li>
-      <br />
-      <br />
       <li style="opacity:.25"><a href="/vault">what's the password</a></li>
     </ul>
   </header>
@@ -72,5 +89,8 @@
 
   main {
     padding: 2rem;
+  }
+  .wip {
+    opacity: 0.5;
   }
 </style>
