318ae80a9c1a5aa57d75370f5293f3418f2c6cbb
dave <dave@davecode.me>
2021-11-14 00:31:50 -0500

    credits page

--- src/routes/credits/index.svelte
+++ src/routes/credits/index.svelte
@@ -3,5 +3,42 @@
 </script>
 
 <main>
-  <!-- {JSON.stringify(yaml)} -->
+  <h1>special thanks &lt;3</h1>
+  <p>these dear friends have made this creative dream possible:</p>
+  <ul>
+    {#each yaml.special_thanks as credit, i}
+      <li style="transform:translateX({(yaml.baked_rng[i] - 0.5) * 200}px)">
+        {credit}
+      </li>
+    {/each}
+  </ul>
+  <p>friendship is part of the key to creativity.</p>
 </main>
+
+<style lang="scss">
+  main {
+    margin: 2rem 0;
+  }
+  h1 {
+    text-align: center;
+    color: #e089c8;
+    text-shadow: shadow(3px, 1, #edb9dd);
+    margin-bottom: 1rem;
+  }
+  p {
+    font-size: 1.5rem;
+    text-align: center;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 2rem 0;
+  }
+  li {
+    font-size: 1.5rem;
+    margin: -0.5rem auto;
+    text-align: center;
+    width: fit-content;
+    color: #9a008a;
+  }
+</style>
