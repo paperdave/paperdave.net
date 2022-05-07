8e2deb140f09c1890bcf5b1036e3e7623e745985
Dave Caruso <dave@davecode.net>
2022-05-07 06:08:51 -0400

    move stuff around

--- src/lib/components/ThemeRoot.svelte
+++ src/lib/components/ThemeRoot.svelte
@@ -2,7 +2,6 @@
   import Color from 'color';
 
   export let dark = false;
-
   export let foreground = dark ? 'white' : 'black';
   export let background = dark ? 'black' : 'white';
   export let accent = '#22c646';
@@ -11,25 +10,25 @@
   let foregroundColor = new Color(foreground);
   let backgroundColor = new Color(background);
   let accentColor = new Color(accent);
-
-  export let padding = false;
 </script>
 
 <theme-root
-  style="
-  --foreground: {Math.round(foregroundColor.hue())},
-    {Math.round(foregroundColor.saturationl())}%,
-    {Math.round(foregroundColor.lightness())}%;
-  --background: {Math.round(backgroundColor.hue())},
-    {Math.round(backgroundColor.saturationl())}%,
-    {Math.round(backgroundColor.lightness())}%;
-  --accent-hue: {Math.round(accentColor.hue())};
-  --accent-saturation: {Math.round(accentColor.saturationl())}%;
-  --accent-lightness: {Math.round(accentColor.lightness())}%;
-  {linkColor ? `--link-color:${linkColor};` : ''}
-"
-  class:padding
-  class:dark
-  grow>
+  style:--foreground={[
+    `${Math.round(foregroundColor.hue())}`,
+    `${Math.round(foregroundColor.saturationl())}%`,
+    `${Math.round(foregroundColor.lightness())}%`,
+  ].join(',')}
+  style:--background={[
+    `${Math.round(backgroundColor.hue())}`,
+    `${Math.round(backgroundColor.saturationl())}%`,
+    `${Math.round(backgroundColor.lightness())}%`,
+  ].join(',')}
+  style:--accent-hue={Math.round(accentColor.hue())}
+  style:--accent-saturation={Math.round(accentColor.saturationl())}
+  style:--accent-lightness={Math.round(accentColor.lightness())}
+  style:--link-color={linkColor}
+  class:dark>
   <slot />
 </theme-root>
+
+<!-- styles are in global.scss -->
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import '../global.scss';
-  import '../theme.scss';
   import Footer from '$lib/components/Footer.svelte';
   import { browser } from '$app/env';
 
@@ -18,10 +17,44 @@
 <main>
   <slot />
 </main>
-<Footer />
 
-<!-- <span class='oob-text'>
-  <a class='custom' href="https://github.com/davecaruso/davecode.net/tree/main/src/global.scss#L17">
+<span class="oob-text">
+  <a class="custom" href="https://github.com/davecaruso/davecode.net/tree/main/src/global.scss#L17">
     why do you have such a high-res screen???
   </a>
-</span> -->
+</span>
+
+<Footer />
+
+<style lang="scss">
+  main {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    overflow: hidden;
+  }
+
+  .oob-text {
+    display: none;
+    z-index: 1337;
+    position: absolute;
+    right: 0;
+    top: 0;
+    transform: rotate(-90deg);
+    transform-origin: top right;
+
+    a {
+      color: white;
+      &:hover {
+        color: red;
+      }
+    }
+  }
+
+  @media (min-width: 3880px) {
+    .oob-text {
+      display: block;
+    }
+  }
+</style>
