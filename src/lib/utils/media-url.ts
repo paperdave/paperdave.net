c3f12f181239ef0dfd9420b7f32971e4fd9531e1
Dave Caruso <dave@davecode.net>
2022-06-06 23:34:48 -0500

    stuff

--- src/lib/utils/media-url.ts
+++ src/lib/utils/media-url.ts
@@ -4,11 +4,7 @@ interface DecodedImage {
   url: string;
   aspect?: string;
   blurhash?: string;
-}
-interface DecodedVideo {
-  url: string;
-  aspect?: string;
-  blurhash?: string;
+  hash?: string;
 }
 
 function gcd(a: number, b: number) {
@@ -37,16 +33,12 @@ function encodeAspect(width: number, height: number) {
   }
 
   const a = `${numerator.toString(32)}:${denominator.toString(32)}`;
-  const b = `${width / height}`.replace(/$0\./, '');
+  const b = `${width / height}`.replace(/$0\./g, '');
 
   return a.length <= b.length ? a : b;
 }
 
-export function encodeImageUrl(hash: string, width: number, height: number, blurhash: string) {
-  return `${hash}/${encodeAspect(width, height)}/${blurhash}`;
-}
-
-export function decodeImageUrl(string: string): DecodedImage {
+function decodeImageUrl(string: string): DecodedImage {
   if (!string) return null;
 
   try {
@@ -63,29 +55,119 @@ export function decodeImageUrl(string: string): DecodedImage {
   }
 
   return {
+    hash,
     url: `${URL_BASE}/${hash.slice(0, 2)}/${hash}.webp`,
     aspect: aspect ? decodeAspect(aspect) : undefined,
     blurhash,
   };
 }
 
-export function decodeVideoUrl(string: string): DecodedVideo {
-  if (!string) return null;
-
-  try {
-    return {
-      url: string.startsWith('/') ? string : new URL(string).toString(),
-    };
-  } catch (error) {
-    //
+const hashCharacters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-';
+export async function hashMediaData(data: ArrayBufferLike) {
+  const bytes = new Uint8Array(await crypto.subtle.digest('sha-256', data));
+  let string = '';
+  for (let i = 0; i < 3 * 5; i += 3) {
+    const a = bytes[i] << 16 | bytes[i + 1] << 8 | bytes[i + 2];
+    string += hashCharacters[a >> 18];
+    string += hashCharacters[(a >> 12) & 63];
+    string += hashCharacters[(a >> 6) & 63];
+    string += hashCharacters[a & 63];
   }
+  return string;
+}
 
-  const [hash] = string.split('/');
-  if (hash.length !== 32) {
-    return null;
+const extensionToTypeChar = {
+  'webp': 'i',
+  'webm': 'v',
+  'mp4': '4',
+  'mp3': '3',
+  'wav': 'w',
+  'flac': 'f',
+  'png': 'p',
+  'jpeg': 'j',
+  'av1': 'a',
+  'avif': 'A',
+};
+
+const imageExtensions = ['webp', 'png', 'avif'];
+
+export type MediaType = keyof typeof extensionToTypeChar;
+
+const typeCharToExtension = Object.keys(extensionToTypeChar).reduce((acc, key: MediaType) => {
+  acc[extensionToTypeChar[key]] = key;
+  return acc;
+}, {} as { [key: string]: MediaType });
+
+export type MediaDataAllowWidthHeight = MediaData | {
+  type: MediaType;
+  hash: string;
+  blurhash?: string;
+  width: number;
+  height: number;
+  duration?: number;
+}
+export type MediaData = {
+  type: MediaType;
+  hash: string;
+  blurhash?: string;
+  aspect: string;
+  duration?: number;
+}
+
+export type DecodedMediaData = MediaData & { url: string };
+
+export function createMediaId(options: MediaDataAllowWidthHeight) {
+  const [width, height] = 'aspect' in options ?
+    options.aspect.includes('/')
+      ? options.aspect.split('/').map(x => parseInt(x, 10))
+      : [parseInt(options.aspect, 10), 1]
+    : [options.width, options.height];
+
+  const aspect = encodeAspect(width, height);
+  let str = `${extensionToTypeChar[options.type]}${options.hash}${aspect}`;
+  if (options.blurhash) {
+    str += `/${options.blurhash}`;
   }
+  if (options.duration) {
+    str += `/${options.duration.toString()}`;
+  }
+  return str;
+}
 
-  return {
-    url: `${URL_BASE}/${hash.slice(0, 2)}/${hash}.webm`,
+export function decodeMediaId(id: string) {
+  const legacy = decodeImageUrl(id);
+  if (legacy) return {
+    type: 'webp',
+    url: legacy.url,
+    hash: legacy.hash,
+    blurhash: legacy.blurhash,
+    aspect: legacy.aspect,
   };
+
+  const firstSlashIndexOf = id.indexOf('/');
+  const endOfAspect = firstSlashIndexOf === -1 ? 21 : firstSlashIndexOf;
+
+  const media: DecodedMediaData = {
+    type: typeCharToExtension[id.slice(0, 1)],
+    hash: id.slice(1, 21),
+    aspect: decodeAspect(id.slice(21, endOfAspect)),
+    url: URL_BASE,
+  };
+  media.url += `/${media.hash[0]}/${media.hash}.${media.type}`;
+
+  if (firstSlashIndexOf !== -1) {
+    const extra = id.slice(endOfAspect).split('/');
+    if (extra.length === 2) {
+      media.blurhash = extra[0];
+      media.duration = parseInt(extra[1], 32);
+    } else if (extra.length === 1) {
+      if (imageExtensions.includes(media.type)) {
+        media.blurhash = extra[0];
+      } else {
+        media.duration = parseInt(extra[0], 32);
+      }
+    }
+  }
+
+  return media;
 }
