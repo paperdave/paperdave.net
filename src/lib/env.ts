1a284bfe67a72c19b6d724d1519531f912928087
Dave Caruso <dave@davecode.net>
2022-06-07 23:25:12 -0500

    huh

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,3 +1,5 @@
 // 
-export const DATABASE_URL = process.env.DATABASE_URL_PROXY ?? process.env.DATABASE_URL;
+export const DATABASE_URL = process.env.DATABASE_URL_PROXY;
 export const PROXYCHECK_API_KEY = process.env.PROXYCHECK_API_KEY;
+export const GIT_COMMIT_ID = process.env.VERCEL_GIT_COMMIT_SHA;
+export const GIT_COMMIT_BRANCH = process.env.VERCEL_GIT_COMMIT_REF;
