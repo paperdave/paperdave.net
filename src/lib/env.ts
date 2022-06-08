861a3c154a8d9ba6364e6755c88bec92147b9a38
Dave Caruso <dave@davecode.net>
2022-06-07 22:54:17 -0500

    env changes

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,9 +1,3 @@
-function envNonEmptyString(name: string, value: string) {
-  if (!value) {
-    throw new Error(`${name} environment variable is required`);
-  }
-  return value;
-}
-
-export const DATABASE_URL = envNonEmptyString('DATABASE_URL', (process.env as any).DATABASE_URL);
-export const PROXYCHECK_API_KEY = (process.env as any).PROXYCHECK_API_KEY;
+// 
+export const DATABASE_URL = process.env.DATABASE_URL_PROXY ?? process.env.DATABASE_URL;
+export const PROXYCHECK_API_KEY = process.env.PROXYCHECK_API_KEY;
