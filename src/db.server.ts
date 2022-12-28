8c36da587533029fdea19a9ab4026902f0cf5267
Dave Caruso <me@paperdave.net>
2022-12-28 00:40:53 -0500

    fix env loading

--- src/db.server.ts
+++ src/db.server.ts
@@ -1,3 +1,10 @@
 import { PrismaClient } from '@prisma/client';
+import * as env from '$env/static/private';
 
-export const db = new PrismaClient();
+export const db = new PrismaClient({
+  datasources: {
+    db: {
+      url: (env as any).DATABASE_URL
+    }
+  }
+});
