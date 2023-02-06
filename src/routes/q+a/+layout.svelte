e813bda88050c126a286e3af805f1ef9b2658a42
Dave Caruso <me@paperdave.net>
2023-02-06 02:21:17 -0500

    stuff

--- src/routes/q+a/+layout.svelte
+++ src/routes/q+a/+layout.svelte
@@ -1,8 +1,10 @@
 <script>
   import { page } from '$app/stores';
   import BackButton from 'src/components/BackButton.svelte';
+  import Link from 'src/lib/link/Link.svelte';
   import ThemeRoot from 'src/lib/theme-root/ThemeRoot.svelte';
   import Header from './_lib/Header.svelte';
+  import RandomizeLink from './_lib/RandomizeLink.svelte';
 </script>
 
 <ThemeRoot background="#111" primary="#faa719" secondary="#ff3a32" foreground="#f0f0f0">
@@ -13,12 +15,19 @@
       <BackButton />
     {/if}
     <Header />
-    <!-- <layout-link-row>
+    <layout-link-row>
       <Link href="/q+a">latest</Link>
-      <Link href="/q+a/search">search</Link>
+      <!-- <Link href="/q+a/search">search</Link> -->
+      <span class="soon">search (soon)</span>
       <RandomizeLink href="/io/random" />
-      <Link href="/q+a?page=0">start</Link>
-    </layout-link-row> -->
+      <Link href="/q+a/info">privacy</Link>
+    </layout-link-row>
     <slot />
   </layout-container>
 </ThemeRoot>
+
+<style>
+  .soon {
+    color: #ffffff77;
+  }
+</style>
